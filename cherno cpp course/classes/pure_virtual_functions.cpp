91636e2556014abf391c1612eb2a8740f0136136
Hamza Boumahdi <hboumahd@e1r4p2.1337.ma>
2022-12-04 11:57:05 +0100

    finish pure virtual functions

---
+++ cherno cpp course/classes/pure_virtual_functions.cpp
@@ -0,0 +1,40 @@
+#include "../header.h"
+
+// if you make the MakeSound() function equal to 0 , it becomes a pure virtual function
+// and the Instrument class becomes an abstract class
+// @definition :
+//      * abstract class: is a class that have at least one pure virtual function.
+
+class Instrument
+{
+public:
+// now, any class that inherite the Instrument class must provide its own implementation
+// of the MakeSound() function.
+    virtual void MakeSound() = 0;
+};
+
+class Piano : public Instrument
+{
+public:
+    void MakeSound()
+    {
+        printf("Piano playing...\n");
+    }
+};
+
+class Gitar : public Instrument
+{
+public:
+    void MakeSound()
+    {
+        printf("Gitar playing...\n");
+    }
+};
+
+int main()
+{
+    Instrument *i1 = new Piano();
+    Instrument *i2 = new Gitar();
+    i1->MakeSound();
+    i2->MakeSound();
+}
